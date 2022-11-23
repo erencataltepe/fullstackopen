b9265d168e09391192d00e1a48fc0d2998f22c4b
Eren Cataltepe <erencataltepe@gmail.com>
2022-11-23 16:28:42 +0300

    Solve exercise 2.2

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,6 +1,11 @@
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ parts }) => {
+  const total = parts.reduce((total, part) => {
+    return part.exercises + total;
+  }, 0);
+
   return (
     <div>
       {parts.map((part) => {
@@ -8,6 +13,7 @@ const Content = ({ parts }) => {
           <Part key={part.id} name={part.name} exercises={part.exercises} />
         );
       })}
+      <Total total={total} />
     </div>
   );
 };
